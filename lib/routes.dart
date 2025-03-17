bc19567f5cf713e94ae91f12e2f0bed95198d27e
huangzhuoran <huangzhuoran@haomo-studio.com>
2025-03-17 20:05:26 +0800

    fix: 删除登录页面

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'LoginPage.dart';
 import 'MyHomePage.dart';
 
 Map<String, WidgetBuilder> routes() {
   return <String, WidgetBuilder>{
     // Full Apps
     '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
-    '/login': (context) => LoginPage(),
   };
 }
